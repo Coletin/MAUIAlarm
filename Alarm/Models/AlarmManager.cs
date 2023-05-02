a4950a4159182023d119d6ed012f97a15f87cefa
GRode <40580626+Coletin@users.noreply.github.com>
2023-05-02 02:01:08 -0300

    all

--- Alarm/Models/AlarmManager.cs
+++ Alarm/Models/AlarmManager.cs
@@ -11,10 +11,20 @@ namespace Alarm.Models {
 
         public AlarmManager() => LoadAlarms();
 
-        public void LoadAlarms() {
+        public void LoadAlarms() {            
             Alarms.Clear();
 
-            Alarms.Add(new Alarm() { Description = "Descripcion", Name = "Nombre" });
+            //Alarms.Add(new Alarm() { Description = "Descripcion", Name = "Nombre" });
+
+            string appRoute = FileSystem.AppDataDirectory;
+
+            IEnumerable<string> files = Directory.EnumerateFiles(appRoute, "*.alarm.txt");
+            foreach (string file in files) {
+                Alarm newA= new Alarm();
+                newA.FileName = file;
+                newA.LoadFromJSON(File.ReadAllText(file));
+                Alarms.Add(newA);
+            }
         }
     }
 }
--- Alarm/Views/AddAlarm.xaml.cs
+++ Alarm/Views/AddAlarm.xaml.cs
@@ -1,9 +1,42 @@
 namespace Alarm.Views;
 
+[QueryProperty(nameof(ItemID), nameof(ItemID))]
 public partial class AddAlarm : ContentPage
 {
-	public AddAlarm()
+    public string ItemID { 
+        set { LoadAlarm(value); } 
+    }
+    public AddAlarm()
 	{
 		InitializeComponent();
+
+        string dataPath = FileSystem.AppDataDirectory;
+        string randomName = $"{Path.GetRandomFileName()}.alarm.txt";
+
+        LoadAlarm(Path.Combine(dataPath,randomName));
 	}
+
+    private async void Save_Clicked(object sender, EventArgs e) {
+        if (this.BindingContext is Models.Alarm alarm)
+            File.WriteAllText(alarm.FileName, alarm.ToJSON());
+
+        await Shell.Current.GoToAsync("..");
+    }
+
+    private async void Delete_Clicked(object sender, EventArgs e) {
+        if (BindingContext is Models.Alarm alarm && File.Exists(alarm.FileName))
+            File.Delete(alarm.FileName);
+        await Shell.Current.GoToAsync("..");    
+    }
+
+    private void LoadAlarm(string Path) {
+        Models.Alarm alarma = new Models.Alarm();
+        alarma.FileName = Path;
+
+        if(File.Exists(Path)) {
+            alarma.LoadFromJSON(File.ReadAllText(Path));
+        }
+
+        this.BindingContext= alarma;
+    }
 }
